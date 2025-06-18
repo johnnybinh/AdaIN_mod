4a85a33d10177faf04750bf0008603584e3141b6
johnnybinh <thebinhnguyen2703@gmail.com>
2025-06-18 13:50:08 +0700

    modify new loss to use canny operator and LBFGS optimizer - rev4.2 - fix

--- net.py
+++ net.py
@@ -246,5 +246,5 @@ class Net(nn.Module):
         loss_s = self.calc_style_loss(g_t_feats[0], style_feats[0])
         loss_e = self.calc_sobel_loss(g_t, content)  # left is con
         for i in range(1, 4):
-            loss_s += self.calc_style_loss(g_t, style_feats[i])
+            loss_s += self.calc_style_loss(g_t_feats[i], style_feats[i])
         return loss_c, loss_s, loss_e
