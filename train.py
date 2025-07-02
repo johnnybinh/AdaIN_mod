1df1e25092594586df13e0c0d2a69f84558fb991
johnnybinh <thebinhnguyen2703@gmail.com>
2025-07-02 11:49:26 +0700

    delay loss total variation

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ for i in tqdm(range(args.max_iter)):
     loss_s = args.style_weight * loss_s
     if i < 10000:
         # 0.05
-        loss_tv = 0 * loss.tv
+        loss_tv = 0 * loss_tv
     else:
         loss_tv = args.tv_weight * loss_tv
     # for now loss tv weight is 1, LEARN: this is belong to hyperparmeter tuning
