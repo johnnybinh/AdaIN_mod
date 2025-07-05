5ab8385f390ceb35ee230823dff49b9c8eabfb2c
johnnybinh <thebinhnguyen2703@gmail.com>
2025-07-05 13:18:09 +0700

    test no loss_lap

--- train.py
+++ train.py
@@ -133,7 +133,7 @@ for i in tqdm(range(args.max_iter)):
     adjust_learning_rate(optimizer, iteration_count=i)
     content_images = next(content_iter).to(device)
     style_images = next(style_iter).to(device)
-    loss_c, loss_s, loss_lap = network(content_images, style_images)
+    loss_c, loss_s = network(content_images, style_images)
     loss_c = args.content_weight * loss_c
     loss_s = args.style_weight * loss_s  # remove loss tv
     # loss_lap = args.lap_weight * loss_lap
